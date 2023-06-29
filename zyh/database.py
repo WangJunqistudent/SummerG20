98f4b5aab09790a5a5cd7a9015d1a42e85d1c028
The Answer <91312400+asdgghjkll@users.noreply.github.com>
2023-06-29 09:19:01 +0800

    Update database.py

--- zyh/database.py
+++ zyh/database.py
@@ -1,4 +1,7 @@
+from flask import Flask, jsonify, request
 import sqlite3
+
+app = Flask(__name__)
 
 # 创建数据库连接
 conn = sqlite3.connect('file_index.db')
@@ -31,3 +34,26 @@ def search(keyword):
     ''', ('%' + keyword + '%',))
     results = cursor.fetchall()
     return results
+
+# 处理查询请求的API
+@app.route('/search', methods=['GET'])
+def search_api():
+    keyword = request.args.get('keyword')
+
+    # 在数据库中根据关键字搜索匹配的结果
+    results = search(keyword)
+
+    response = jsonify(results)
+    response.headers.add('Access-Control-Allow-Origin', '*')  # 处理跨域请求
+    return response
+
+# 处理保存选中结果的API
+@app.route('/save', methods=['POST'])
+def save():
+    data = request.get_json()
+
+    # 将选中结果保存到文本文件并提供下载链接
+    # ...
+
+if __name__ == '__main__':
+    app.run()
