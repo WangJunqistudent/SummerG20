db4e0f45f898f36ce552289f144d7723f8f976b1
The Answer <91312400+asdgghjkll@users.noreply.github.com>
2023-06-29 09:22:41 +0800

    Update app.py

--- zyh/app.py
+++ zyh/app.py
@@ -1,27 +1,65 @@
-
 from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
-# 处理查询请求的API
-@app.route('/search', methods=['GET'])
-def search():
-    keyword = request.args.get('keyword')
+# 在这里创建数据库连接
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('database.db')  # 数据库文件名为 database.db
+        print('数据库连接成功')
+    except sqlite3.Error as e:
+        print(e)
+    return conn
 
-    # 在数据库中根据关键字搜索匹配的结果
-    results = db.search(keyword)  # 假设db是一个数据库操作对象
+# 在这里创建数据库表
+def create_table(conn):
+    try:
+        cursor = conn.cursor()
+        create_table_query = '''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
+        '''
+        cursor.execute(create_table_query)
+        conn.commit()
+        print('数据库表创建成功')
+    except sqlite3.Error as e:
+        print(e)
 
-    response = jsonify(results)
-    response.headers.add('Access-Control-Allow-Origin', '*')  # 处理跨域请求
-    return response
+# 路由：获取所有用户
+@app.route('/users', methods=['GET'])
+def get_users():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    users = []
+    for row in rows:
+        user = {
+            'id': row[0],
+            'name': row[1],
+            'email': row[2]
+        }
+        users.append(user)
+    return jsonify(users)
 
-# 处理保存选中结果的API
-@app.route('/save', methods=['POST'])
-def save():
-    data = request.get_json()
+# 路由：创建新用户
+@app.route('/users', methods=['POST'])
+def create_user():
+    conn = create_connection()
+    cursor = conn.cursor()
+    name = request.form.get('name')
+    email = request.form.get('email')
+    cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", (name, email))
+    conn.commit()
+    return '用户创建成功'
 
-    # 将选中结果保存到文本文件并提供下载链接
-    # ...
 
 if __name__ == '__main__':
+    conn = create_connection()
+    create_table(conn)
     app.run()
